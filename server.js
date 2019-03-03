8b7de1c750fbd011074fa54175128cdd12f0b30c
unknown <slupton89@gmail.com>
2019-03-02 20:02:34 -0700

    server is correctly fetching weather data based on coords

--- server.js
+++ server.js
@@ -13,22 +13,33 @@ app.use(weather)
 const server = http.createServer(app)
 const io = socketIo(server)
 
-const getApiAndEmit = async socket => {
-  try {
-    const res = await axios.get(`https://api.darksky.net/forecast/${DARKSKY_API_KEY}/43.7695,11.2558`)
-    socket.emit('FromAPI', res.data.currently.temperature)
-  } catch (error) {
-    console.error(`Error: $(error.code)`)
-  }
-}
+let interval
+let userCoords
 
 io.on('connection', socket => {
   console.log('New client connected')
+  socket.on('message', (loc) => {
+    userCoords = loc
+    console.log(userCoords)
+    return getApiAndEmit(socket)
+  })
   if(interval) {
     clearInterval(interval)
   }
-  interval = setInterval(() => getApiAndEmit(socket), 10000)
+  interval = setInterval(() => getApiAndEmit(socket), 1800000)
   socket.on('disconnect', () => console.log('Client disconnected'))
 })
+
+const getApiAndEmit = async socket => {
+  console.log('fetching')
+  try {
+    const res = await axios.get(`https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${userCoords[0]},${userCoords[1]}`)
+    socket.emit('FromAPI', res.data.currently.temperature)
+  } catch (error) {
+    console.error(`Error: ${error.code}`)
+  }
+}
+
+
 
 server.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
