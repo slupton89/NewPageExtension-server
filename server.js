5d0482bcc1c10639c52b88046fd34afce82b107d
unknown <slupton89@gmail.com>
2019-03-02 18:19:26 -0700

    added darksky api calls and configured the server

--- server.js
+++ server.js
@@ -13,4 +13,22 @@ app.use(weather)
 const server = http.createServer(app)
 const io = socketIo(server)
 
-const getApiAndEmit = 'TODO'
+const getApiAndEmit = async socket => {
+  try {
+    const res = await axios.get(`https://api.darksky.net/forecast/$(DARKSKY_API_KEY)/43.7695,11.2558`)
+    socket.emit('FromAPI', res.data.currently.temperature)
+  } catch (error) {
+    console.error(`Error: $(error.code)`)
+  }
+}
+
+io.on('connection', socket => {
+  console.log('New client connected')
+  if(interval) {
+    clearInterval(interval)
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 10000)
+  socket.on('disconnect', () => console.log('Client disconnected'))
+})
+
+server.listen(port, () => console.log(`Listening on port $(port)`))
