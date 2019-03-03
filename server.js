b9a23c4740a05d20ed0de11f33af926219530b82
Shane Lupton <slupton89@icloud.com>
2019-03-03 00:38:32 -0700

    can't deploy servers I guess

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const http = require('http')
-const socketIo = require('socket.io')
-const axios = require('axios')
-const cors = require('cors')
+// const http = require('http')
+// const socketIo = require('socket.io')
+// const axios = require('axios')
+// const cors = require('cors')
 
 // const { PORT, DARKSKY_API_KEY } = require('./config')
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
@@ -12,50 +12,50 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 const app = express()
 
-app.use(
-  cors({
-    origin: 'http://localhost:3000'
-  })
-)
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000'
+//   })
+// )
 // app.use(weather)
 
-const server = http.createServer(app)
-const io = socketIo(server)
+// const server = http.createServer(app)
+// const io = socketIo(server)
 
 app.get('/', (req, res) => {
-  res.send({ response: 'Working'}).status(200)
-  res.end('Nothing to see here')
+  // res.send({ response: 'Working'}).status(200)
+  res.send('Nothing to see here')
 })
 
-let interval
-let userCoords
+// let interval
+// let userCoords
 
-io.on('connection', socket => {
-  console.log('New client connected')
-  socket.on('message', (loc) => {
-    userCoords = loc
-    console.log(userCoords)
-    return getApiAndEmit(socket)
-  })
-  if(interval) {
-    clearInterval(interval)
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1800000)
-  socket.on('disconnect', () => console.log('Client disconnected'))
-})
+// io.on('connection', socket => {
+//   console.log('New client connected')
+//   socket.on('message', (loc) => {
+//     userCoords = loc
+//     console.log(userCoords)
+//     return getApiAndEmit(socket)
+//   })
+//   if(interval) {
+//     clearInterval(interval)
+//   }
+//   interval = setInterval(() => getApiAndEmit(socket), 1800000)
+//   socket.on('disconnect', () => console.log('Client disconnected'))
+// })
 
-const getApiAndEmit = async socket => {
-  console.log('fetching')
-  try {
-    const res = await axios.get(`https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${userCoords[0]},${userCoords[1]}`)
-    socket.emit('FromAPI', res.data.currently)
-  } catch (error) {
-    console.error(`Error: ${error.code}`)
-  }
-}
+// const getApiAndEmit = async socket => {
+//   console.log('fetching')
+//   try {
+//     const res = await axios.get(`https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${userCoords[0]},${userCoords[1]}`)
+//     socket.emit('FromAPI', res.data.currently)
+//   } catch (error) {
+//     console.error(`Error: ${error.code}`)
+//   }
+// }
 
 
 
-server.listen(server_port, server_ip_address, function () {
+app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
