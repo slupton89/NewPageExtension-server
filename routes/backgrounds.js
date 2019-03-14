9a58a7be64bfdb892ede1170837c92313e9016d2
Shane Lupton <slupton89@icloud.com>
2019-03-13 23:42:28 -0600

    api ready to connect with front-end

--- routes/backgrounds.js
+++ routes/backgrounds.js
@@ -8,21 +8,21 @@ const Flickr = require('flickrapi'),
 
 const backgroundRouter = express.Router()
 
-backgroundRouter.get('/bg', (req, res) => {
-  res.send('Nothing to see here 🙂').status(200)
-})
-
-
-backgroundRouter.get('/bg/space', (req, res, next) => {
-  Flickr.tokenOnly(flickerOptions, (error, flickr) => {
-    flickr.photos.search({
-      text: 'space landscape',
-      per_page: 1
-    }, (err, result) => {
-          err === true ? res.send({ error: err }).status(500)
-                       : res.send(result.photos.photo)
-      })
-  })
+backgroundRouter.get('/bg', (req, res, next) => {
+  if(req.query.type) {
+    Flickr.tokenOnly(flickerOptions, (error, flickr) => {
+      flickr.photos.search({
+        text: req.query.type,
+        per_page: 10
+      }, (err, result) => {
+            err === true ? res.send({ error: err }).status(500)
+                        : res.send(result.photos.photo)
+        })
+        error !== null ? console.log(error) : null
+    })
+  } else {
+    res.send('Nothing to see here 🙂').status(200)
+  }
 })
 
 module.exports = backgroundRouter
