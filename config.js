8c84eee08da729339ac522b92793bada80b9cedc
Shane Lupton <slupton89@icloud.com>
2019-03-13 23:02:15 -0600

    added flickr config and endpoint

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 
 module.exports = {
+  FLICKR_API_KEY: process.env.FLICKR_API_KEY,
+  FLICKR_SECRET: process.env.FLICKR_SECRET,
   DARKSKY_API_KEY: process.env.DARKSKY_API_KEY,
   PORT: process.env.PORT
 }
--- routes/backgrounds.js
+++ routes/backgrounds.js
@@ -1,8 +1,29 @@
 const express = require('express')
+const { FLICKR_API_KEY, FLICKR_SECRET } = require('../config')
+const Flickr = require('flickrapi'),
+      flickerOptions = {
+        api_key: FLICKR_API_KEY,
+        secret: FLICKR_SECRET
+      }
 
 const backgroundRouter = express.Router()
 
-backgroundRouter.get('/bg/:bgType' (req, res, next) => {
+backgroundRouter.get('/bg', (req, res) => {
+  res.send({ response: 'Space!'}).status(200)
+})
+
+
+backgroundRouter.get('/bg/space/', (req, res, next) => {
+  console.log('space!')
+  res.send('Yup')
+  Flickr.tokenOnly(flickerOptions, (error, flickr) => {
+    flickr.photos.search({
+      text: 'space landscape',
+      per_page: 1
+    }, (err, result) => {
+          console.log(err !== null ? err : result.photo)
+        })
+  })
 
 })
 
