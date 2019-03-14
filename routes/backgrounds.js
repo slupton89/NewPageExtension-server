2d02e71a94c8665b27b025879a8b10f472306e23
Shane Lupton <slupton89@icloud.com>
2019-03-13 22:10:24 -0600

    added background router for retrieving images

---
+++ routes/backgrounds.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const backgroundRouter = express.Router()
+
+backgroundRouter.get('/bg/:bgType' (req, res, next) => {
+
+})
+
+module.exports = backgroundRouter
